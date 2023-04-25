add730392e5d450f9cb079484087498ae0d20159
jeonghwanso <thwjdghks1023@gmail.com>
2023-04-25 16:38:49 +0900

    Promotion, PromotionProduct 레포지토리 추가

---
+++ src/main/java/antigravity/repository/PromotionProductsRepository.java
@@ -0,0 +1,32 @@
+package antigravity.repository;
+
+import antigravity.domain.entity.PromotionProducts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class PromotionProductsRepository {
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public List<PromotionProducts> getPromotionProductsList(int product_id) {
+        String query = "SELECT * FROM `promotion_products` WHERE id = :product_id ";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("product_id", product_id);
+
+        return namedParameterJdbcTemplate.query(
+                query,
+                params,
+                (rs, rowNum) -> PromotionProducts.builder()
+                        .id(rs.getInt("id"))
+                        .promotionId(rs.getInt("promotion_id"))
+                        .productId(rs.getInt("product_id"))
+                        .build()
+        );
+    }
+}
---
+++ src/main/java/antigravity/repository/PromotionRepository.java
@@ -0,0 +1,36 @@
+package antigravity.repository;
+
+import antigravity.domain.entity.Promotion;
+import antigravity.domain.type.DiscountType;
+import antigravity.domain.type.PromotionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class PromotionRepository {
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public Promotion getPromotion(int id) {
+        String query = "SELECT * FROM `promotion` WHERE id = :id ";
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", id);
+
+        return namedParameterJdbcTemplate.queryForObject(
+                query,
+                params,
+                (rs, rowNum) -> Promotion.builder()
+                        .id(rs.getInt("id"))
+                        .promotion_type(PromotionType.valueOf(rs.getString("promotion_type")))
+                        .name(rs.getString("name"))
+                        .discount_type(DiscountType.valueOf(rs.getString("discount_type")))
+                        .discount_value(rs.getInt(rs.getInt("discount_value")))
+                        .use_started_at(rs.getDate("use_started_at"))
+                        .use_ended_at(rs.getDate("use_ended_at"))
+                        .build()
+        );
+    }
+}
