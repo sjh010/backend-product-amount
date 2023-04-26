64aca36d9d308f53a42b4810c657903a1a4c2961
jeonghwanso <thwjdghks1023@gmail.com>
2023-04-27 00:35:54 +0900

    Optional 적용

--- src/main/java/antigravity/repository/ProductRepository.java
+++ src/main/java/antigravity/repository/ProductRepository.java
@@ -2,29 +2,39 @@ package antigravity.repository;
 
 import antigravity.domain.entity.Product;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Repository
+@Slf4j
 public class ProductRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    public Product getProduct(int id) {
+    public Optional<Product> getProduct(int id) {
         String query = "SELECT * FROM `product` WHERE id = :id ";
 
         MapSqlParameterSource params = new MapSqlParameterSource();
         params.addValue("id", id);
 
-        return namedParameterJdbcTemplate.queryForObject(
-                query,
-                params,
-                (rs, rowNum) -> Product.builder()
-                        .id(rs.getInt("id"))
-                        .name(rs.getString("name"))
-                        .price(rs.getInt("price"))
-                        .build()
-        );
+        try {
+            return Optional.ofNullable(namedParameterJdbcTemplate.queryForObject(
+                    query,
+                    params,
+                    (rs, rowNum) -> Product.builder()
+                            .id(rs.getInt("id"))
+                            .name(rs.getString("name"))
+                            .price(rs.getInt("price"))
+                            .build()
+            ));
+        } catch (DataAccessException e) {
+            log.error("product repository exception", e);
+            return Optional.empty();
+        }
     }
 }
